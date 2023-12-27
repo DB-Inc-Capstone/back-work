e29166e3119cc5c9dbfeab6d7061ea3333379e35
uhj130 <smtbhj@naver.com>
2023-12-27 16:42:35 +0900

    remove .git files

---
+++ back-work/src/test/java/kr/co/dbinc/backwork/BackWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dbinc.backwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
