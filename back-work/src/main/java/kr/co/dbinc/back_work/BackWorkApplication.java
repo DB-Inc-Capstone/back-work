d1fd21dc00d5fd6bdadaadb85bd4b109fd546aa8
uhj130 <smtbhj@naver.com>
2024-05-17 15:38:36 +0900

    update code

--- back-work/src/main/java/kr/co/dbinc/back_work/BackWorkApplication.java
+++ back-work/src/main/java/kr/co/dbinc/back_work/BackWorkApplication.java
@@ -4,7 +4,9 @@ package kr.co.dbinc.back_work;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan(basePackages = "kr.co.dbinc.back_work.mapper")
 public class BackWorkApplication {
