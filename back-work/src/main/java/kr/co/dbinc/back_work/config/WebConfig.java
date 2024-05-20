bd1f74282c49090dbd3751359f77404738378bf8
DevTae <dev.taehyeon.kim@gmail.com>
2024-05-20 14:28:38 +0900

    Update config codes to fix cors problem

---
+++ back-work/src/main/java/kr/co/dbinc/back_work/config/WebConfig.java
@@ -0,0 +1,20 @@
+package kr.co.dbinc.back_work.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// security 이외의 상황
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+	
+}
